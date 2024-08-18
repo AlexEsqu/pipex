c996c16871647791c09a91e1671d461d73c45213
Alex Kling <mkling@student.42.fr>
2024-08-18 17:02:35 +0200

    FIXED one leak when parsing cmd path

--- src/pipex_parsing.c
+++ src/pipex_parsing.c
@@ -6,34 +6,54 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 15:36:04 by mkling            #+#    #+#             */
-/*   Updated: 2024/08/18 16:13:06 by mkling           ###   ########.fr       */
+/*   Updated: 2024/08/18 17:02:12 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void free_array(char **array)
+{
+    int i = 0;
+
+    if (array == NULL)
+        return;
+
+    while (array[i] != NULL)
+    {
+        free(array[i]);
+        i++;
+    }
+    free(array);
+}
+
+
 int	find_viable_path(t_command *cmd, char *paths)
 {
 	char	*tested_path;
 	char	**possible_paths;
+	int		i;
 
 	possible_paths = ft_split(paths, ':');
 	free(paths);
-	while (*possible_paths++ != NULL)
+	i = 0;
+	while (possible_paths[i] != NULL)
 	{
-		if (*possible_paths == NULL)
+		if (possible_paths[i] == NULL)
 			break ;
-		tested_path = ft_strjoin(*possible_paths, cmd->cmd_stem);
+		tested_path = ft_strjoin(possible_paths[i], cmd->cmd_stem);
 		if (access(tested_path, F_OK | R_OK) == 0)
 		{
+			free_array(possible_paths);
 			cmd->cmd_path = tested_path;
 			return (0);
 		}
 		else
 			free(tested_path);
+		i++;
 	}
-	perror("No viable path found to this command");
-	return (1);
+	free_array(possible_paths);
+	return (perror("No viable path found to this command"), 1);
 }
 
 int	find_cmd_path(char **envp, t_command *cmd)
@@ -52,8 +72,7 @@ int	find_cmd_path(char **envp, t_command *cmd)
 		}
 	}
 	cmd->cmd_path = NULL;
-	perror("No PATH variable found");
-	return (1);
+	return (perror("No PATH variable found"), 1);
 }
 
 int	parse_cmd(char *cmd_line, char **envp, t_command *cmd)
