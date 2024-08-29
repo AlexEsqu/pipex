8e4dbfbfbe538c2a0d03a6d33facf1573443eac9
Alex Kling <momiji.kling@gmail.com>
2024-08-29 16:16:16 +0200

    trying out

--- src/pipex.c
+++ src/pipex.c
@@ -17,18 +17,9 @@ int	execute_cmd_tru_pipe(int pipe_fd[], char **argv, char **envp, int index)
 	char	**cmd_argv;
 	char	*cmd_path;
 
-	if (index == CMD_1)
-	{
-		if (dup2(pipe_fd[READ], STDIN_FILENO) == -1)
-			return (perror("Error while redirecting pipe read to stdin"), 1);
-	}
-	if (index != CMD_1)
-	{
-		if (dup2(pipe_fd[WRITE], STDOUT_FILENO) == -1)
-			return (perror("Error while redirecting pipe write to stdout"), 1);
-	}
+	if (dup2(pipe_fd[WRITE], STDOUT_FILENO) == -1)
+		return (perror("Error while redirecting pipe write to stdout"), -1);
 	close(pipe_fd[READ]);
-	close(pipe_fd[WRITE]);
 	cmd_argv = get_cmd_argv(argv[index]);
 	cmd_path = get_cmd_path(cmd_argv[0], envp);
 	execve(cmd_path, cmd_argv, envp);
@@ -48,7 +39,8 @@ int	pipe_and_fork(char **argv, char **envp, int cmd_index, int *exit_status)
 	if (fork_pid == 0)
 		execute_cmd_tru_pipe(pipe_fd, argv, envp, cmd_index);
 	close(pipe_fd[WRITE]);
-	dup2(pipe_fd[READ], STDOUT_FILENO);
+	if (dup2(pipe_fd[READ], STDIN_FILENO) == -1)
+		return (perror("Error while redirecting pipe read to stdin"), -1);
 	waitpid(fork_pid, exit_status, 0);
 	return (0);
 }
@@ -61,12 +53,12 @@ int	set_files_as_stdin_n_out(char *infile_path, char *outfile_path)
 	infile_fd = open(infile_path, O_RDONLY);
 	if (infile_fd == -1)
 		perror("Error while opening infile");
+	else if (dup2(infile_fd, STDIN_FILENO) == -1)
+		return (perror("Error while redirecting infile to stdin"), 1);
 	outfile_fd = open(outfile_path, O_WRONLY | O_TRUNC | O_CREAT);
 	if (outfile_fd == -1)
-		return (perror("Error while opening outfile"), 1);
-	if (dup2(infile_fd, STDIN_FILENO) == -1)
-		return (perror("Error while redirecting infile to stdin"), 1);
-	if (dup2(outfile_fd, STDOUT_FILENO) == -1)
+		return (perror("Error while opening outfile"), -1);
+	else if (dup2(outfile_fd, STDOUT_FILENO) == -1)
 		return (perror("Error while redirecting stdout to outfile"), 1);
 	return (0);
 }
@@ -82,7 +74,7 @@ int	main(int argc, char **argv, char *envp[])
 		if (set_files_as_stdin_n_out(argv[1], argv[argc - 1]) == -1)
 			return (-1);
 		cmd_index = CMD_1;
-		while (cmd_index < argc - 1)
+		while (cmd_index < argc - 2)
 		{
 			if (pipe_and_fork(argv, envp, cmd_index, &exit_status) == -1)
 				return (-1);
