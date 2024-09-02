5313be2552534ef10440c09bc24334078fe7b673
Alex Kling <mkling@student.42.fr>
2024-09-02 15:30:47 +0200

    clusterfork

--- src/pipex.c
+++ src/pipex.c
@@ -6,39 +6,104 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:59:07 by mkling            #+#    #+#             */
-/*   Updated: 2024/08/28 17:50:38 by mkling           ###   ########.fr       */
+/*   Updated: 2024/09/02 15:28:46 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int	open_file(char *filepath, int mode)
+{
+	int	file_fd;
+
+	if (mode == READ)
+		file_fd = open(file_fd, O_RDWR);
+	else
+		file_fd = open(file_fd, O_WRONLY | O_TRUNC | O_CREAT);
+	if (file_fd == -1)
+		return (perror("Error while opening file"), -1);
+	return (file_fd);
+}
+
+int	redirect(int newfd, int oldfd)
+{
+	if (dup2(newfd, oldfd) == -1)
+		return (perror("Error while redirecting to file"), -1);
+	close(newfd);
+	return (0);
+}
 
 int	execute_cmd_tru_pipe(int pipe_fd[], char **argv, char **envp, int index)
 {
 	char	**cmd_argv;
 	char	*cmd_path;
 
-	if (index == CMD_1)
-	{
-		if (dup2(pipe_fd[READ], STDIN_FILENO) == -1)
-			return (perror("Error while redirecting pipe read to stdin"), 1);
-	}
-	if (index != CMD_1)
-	{
-		if (dup2(pipe_fd[WRITE], STDOUT_FILENO) == -1)
-			return (perror("Error while redirecting pipe write to stdout"), 1);
-	}
 	close(pipe_fd[READ]);
-	close(pipe_fd[WRITE]);
 	cmd_argv = get_cmd_argv(argv[index]);
 	cmd_path = get_cmd_path(cmd_argv[0], envp);
+	if (redirect(pipe_fd[WRITE], STDOUT_FILENO) == -1 || cmd_path == NULL)
+		exit(-1);
 	execve(cmd_path, cmd_argv, envp);
 	return (perror("Error while executing command"), 127);
 }
 
-int	pipe_and_fork(char **argv, char **envp, int cmd_index, int *exit_status)
+// int	exec_first_cmd_from_infile_to_pipe(int pipe_fd[], char **argv,
+// 	char **envp, int cmd_index)
+// {
+// 	char	**cmd_argv;
+// 	char	*cmd_path;
+
+// 	close(pipe_fd[READ]);
+// 	if (dup2(pipe_fd[WRITE], STDOUT_FILENO) == -1)
+// 		return (perror("Error while redirecting pipe write to stdout"), -1);
+// 	close(pipe_fd[WRITE]);
+// 	cmd_argv = get_cmd_argv(argv[cmd_index]);
+// 	cmd_path = get_cmd_path(cmd_argv[0], envp);
+// 	if (cmd_argv == NULL || cmd_path == NULL)
+// 		exit(1);
+// 	execve(cmd_path, cmd_argv, envp);
+// 	return (perror("Error while executing reading command"), 127);
+// }
+
+// int	exec_middle_cmd_from_pipe_to_pipe(int pipe_fd[], char **argv,
+// 	char **envp, int cmd_index)
+// {
+// 	char	**cmd_argv;
+// 	char	*cmd_path;
+
+// 	close(pipe_fd[WRITE]);
+// 	cmd_argv = get_cmd_argv(argv[cmd_index]);
+// 	cmd_path = get_cmd_path(cmd_argv[0], envp);
+// 	execve(cmd_path, cmd_argv, envp);
+// 	return (perror("Error while executing reading command"), 127);
+// }
+
+// int	exec_last_cmd_from_pipe_to_outfile(int pipe_fd[], char **argv,
+// 	char **envp, int cmd_index)
+// {
+// 	int		outfile_fd;
+// 	char	**cmd_argv;
+// 	char	*cmd_path;
+
+// 	redirect_to_filepath(argv[argc])
+// 	close(pipe_fd[WRITE]);
+// 	if (dup2(pipe_fd[READ], STDIN_FILENO) == -1)
+// 		return (perror("Error while redirecting pipe read to stdin"), 1);
+// 	close(pipe_fd[READ]);
+// 	if (dup2(outfile_fd, STDOUT_FILENO) == -1)
+// 		return (perror("Error while redirecting stdout to outfile"), 1);
+// 	close(outfile_fd);
+// 	cmd_argv = get_cmd_argv(argv[cmd_index]);
+// 	cmd_path = get_cmd_path(cmd_argv[0], envp);
+// 	execve(cmd_path, cmd_argv, envp);
+// 	return (perror("Error while executing reading command"), 127);
+// }
+
+int	lay_pipe(char **argv, char **envp, int cmd_index)
 {
 	int	pipe_fd[2];
 	int	fork_pid;
+	int	exit_status;
 
 	if (pipe(pipe_fd) == -1)
 		return (perror("Error while creating pipe"), -1);
@@ -46,45 +111,51 @@ int	pipe_and_fork(char **argv, char **envp, int cmd_index, int *exit_status)
 	if (fork_pid == -1)
 		return (perror("Error while forking"), -1);
 	if (fork_pid == 0)
-		execute_cmd_tru_pipe(pipe_fd, argv, envp, cmd_index);
+		exec_cmd_thru_pipe(pipe_fd, argv, envp, cmd_index);
+	close(pipe_fd[READ]);
 	close(pipe_fd[WRITE]);
-	dup2(pipe_fd[READ], STDOUT_FILENO);
-	waitpid(fork_pid, exit_status, 0);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	waitpid(fork_pid, &exit_status, 0);
 	return (0);
 }
 
-int	set_files_as_stdin_n_out(char *infile_path, char *outfile_path)
-{
-	int	infile_fd;
-	int	outfile_fd;
+// int	set_files_as_stdin_n_out(char *infile_path, char *outfile_path)
+// {
+// 	int	infile_fd;
+// 	int	outfile_fd;
 
-	infile_fd = open(infile_path, O_RDONLY);
-	if (infile_fd == -1)
-		perror("Error while opening infile");
-	outfile_fd = open(outfile_path, O_WRONLY | O_TRUNC | O_CREAT);
-	if (outfile_fd == -1)
-		return (perror("Error while opening outfile"), 1);
-	if (dup2(infile_fd, STDIN_FILENO) == -1)
-		return (perror("Error while redirecting infile to stdin"), 1);
-	if (dup2(outfile_fd, STDOUT_FILENO) == -1)
-		return (perror("Error while redirecting stdout to outfile"), 1);
-	return (0);
-}
+// 	infile_fd = open(infile_path, O_RDWR);
+// 	if (infile_fd == -1)
+// 		perror("Error while opening infile");
+// 	outfile_fd = open(outfile_path, O_WRONLY | O_TRUNC | O_CREAT);
+// 	if (outfile_fd == -1)
+// 		return (perror("Error while opening outfile"), 1);
+// 	if (dup2(infile_fd, STDIN_FILENO) == -1)
+// 		return (perror("Error while redirecting infile to stdin"), 1);
+// 	if (dup2(outfile_fd, STDOUT_FILENO) == -1)
+// 		return (perror("Error while redirecting stdout to outfile"), 1);
+// 	close(infile_fd);
+// 	close(outfile_fd);
+// 	return (0);
+// }
 
 int	main(int argc, char **argv, char *envp[])
 {
 	int	cmd_index;
+	int	infile_fd;
 	int	exit_status;
 
 	exit_status = 0;
-	if (argc >= 5)
+	cmd_index = CMD_1;
+	while (argc >= 5)
 	{
-		if (set_files_as_stdin_n_out(argv[1], argv[argc - 1]) == -1)
+		infile_fd = open_file(argv[INFILE], READ);
+		if (infile_fd == -1 || redirect_to_file(infile_fd, STDIN_FILENO) == -1)
 			return (-1);
-		cmd_index = CMD_1;
 		while (cmd_index < argc - 1)
 		{
-			if (pipe_and_fork(argv, envp, cmd_index, &exit_status) == -1)
+			if (lay_pipe(argc, argv, envp, cmd_index) == -1)
 				return (-1);
 			cmd_index++;
 		}
--- src/pipex_parsing.c
+++ src/pipex_parsing.c
@@ -6,13 +6,13 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 15:36:04 by mkling            #+#    #+#             */
-/*   Updated: 2024/08/28 17:43:59 by mkling           ###   ########.fr       */
+/*   Updated: 2024/09/02 13:53:13 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	free_array(char **array)
+/*static void	free_array(char **array)
 {
 	int	i;
 
@@ -25,7 +25,7 @@ static void	free_array(char **array)
 		free(array[i]);
 	}
 	free(array);
-}
+}*/
 
 static char	*extract_path_variable(char **envp)
 {
@@ -42,30 +42,33 @@ static char	*extract_path_variable(char **envp)
 			return (paths);
 		}
 	}
-	return (perror("No PATH variable found"), NULL);
+	return (NULL);
 }
 
-static char	*return_accessible_path(char *path_variable, char *cmd)
+static char	*return_accessible_path(char **envp, char *cmd)
 {
+	char	*path_variable;
 	char	**possible_paths;
 	char	*tested_path;
 
+	path_variable = extract_path_variable(envp);
+	if (path_variable == NULL)
+		return (perror("No PATH variable found"), NULL);
 	possible_paths = ft_split(path_variable, ':');
 	free(path_variable);
-	while (*possible_paths++ != NULL)
+	while (*possible_paths != NULL)
 	{
-		tested_path = ft_strjoin(*possible_paths, cmd);
+		tested_path = ft_strjoin(*possible_paths++, cmd);
 		if (!tested_path)
 			return (perror("Failed to allocate for tested command path"), NULL);
 		if (access(tested_path, F_OK | R_OK) == 0)
 		{
-			free_array(possible_paths);
+			//free_array(possible_paths); TROUBLE TO FREE ARRAY
 			return (tested_path);
 		}
-		else
-			free(tested_path);
+		free(tested_path);
 	}
-	free_array(possible_paths);
+	//free_array(possible_paths);
 	return (perror("No viable path found to this command"), NULL);
 }
 
@@ -89,8 +92,9 @@ char	*get_cmd_path(char *cmd, char **envp)
 	cmd = ft_strjoin("/", cmd);
 	if (!cmd)
 		return (perror("Failed to allocate the command string"), NULL);
+	// fprintf(stderr, "cmd stem is = %s\n", cmd);
 	if (access(cmd, F_OK | R_OK) == 0)
 		return (cmd);
-	cmd_path = (return_accessible_path(extract_path_variable(envp), cmd_path));
+	cmd_path = return_accessible_path(envp, cmd);
 	return (cmd_path);
 }
